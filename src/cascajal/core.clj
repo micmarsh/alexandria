447f1108f2c3db4eee7edaf15e388a42be1411f0
micmarsh <michael.marsh42@gmail.com>
2013-12-07 13:28:44 -0500

    need a "gensym" thing (#) to include a symbol in a macro

--- src/cascajal/core.clj
+++ src/cascajal/core.clj
@@ -6,16 +6,35 @@
 
 (def-alias Reader nl.siegmann.epublib.epub.EpubReader)
 (def-alias Book nl.siegmann.epublib.domain.Book)
+(def-alias Resource nl.siegmann.epublib.domain.Resource)
 
 (ann reader Reader)
 (def ^:private reader (EpubReader.))
 
+(defmacro defjava [name args body]
+    `(defn ~name ~args
+        (try
+            (~@body)
+            (catch Exception e#
+                (identity nil)))))
+
 ;(ann .readEpub [Reader java.io.FileInputStream -> Book])
-(ann ^:no-check open-book [String -> Book])
-(defn open-book [name]
+(ann ^:no-check open-book [String -> (Option Book)])
+(defjava open-book [name]
     (.readEpub reader
         (java.io.FileInputStream. name)))
 
+;(ann Book/getContents [-> (Seqable Resource)])
+(ann ^:no-check contents [Book -> (Option (Seqable Resource))])
+(defn contents [book]
+    (.getContents book))
+
+(ann do-stuff [Book -> nil])
+(defn do-stuff [b] (println (contents b)))
+
 (ann -main [-> nil])
 (defn -main []
-    (println "yo"))
+    (let [book-name "samples/19d84.epub"
+          book (open-book book-name)]
+          (if book (do-stuff book)
+            (println "error opening book"))))
