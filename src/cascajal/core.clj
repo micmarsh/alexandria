1eb1f30710bb6a5025d5b4740ae74747cf9b9e6f
micmarsh <michael.marsh42@gmail.com>
2013-12-07 12:49:53 -0500

    need to :no-check for now b/c of silly java method

--- src/cascajal/core.clj
+++ src/cascajal/core.clj
@@ -4,6 +4,18 @@
 ;epublib
 (import [nl.siegmann.epublib.epub EpubReader])
 
+(def-alias Reader nl.siegmann.epublib.epub.EpubReader)
+(def-alias Book nl.siegmann.epublib.domain.Book)
+
+(ann reader Reader)
+(def ^:private reader (EpubReader.))
+
+;(ann .readEpub [Reader java.io.FileInputStream -> Book])
+(ann ^:no-check open-book [String -> Book])
+(defn open-book [name]
+    (.readEpub reader
+        (java.io.FileInputStream. name)))
+
 (ann -main [-> nil])
 (defn -main []
     (println "yo"))
